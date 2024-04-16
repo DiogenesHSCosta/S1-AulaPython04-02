b954c7d9533ce6561aa57abb5bcf32133fb6a2f5
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-16 19:57:16 -0300

    inserção enuciandos 1/2/3

--- ex1.py
+++ ex1.py
@@ -1,4 +1,8 @@
-'exercicio 1'
+'''exercicio 1
+
+Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem
+caso o valor seja inválido e continue pedindo até que o usuário informe um valor
+válido'''
 
 while True:
     valor = input("Insira um numero entre 0 e 10: ")
--- ex3.py
+++ ex3.py
@@ -1,4 +1,11 @@
-'exercicio 2'
+#exercicio 2
+
+''' Supondo que a população de um país A seja da ordem de 80000 habitantes com
+uma taxa anual de crescimento de 3% e que a população de B seja 200000
+habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e
+escreva o número de anos necessários para que a população do país A ultrapasse
+ou iguale a população do país B, mantidas as taxas de crescimento
+'''
 
 populacaoA = 80000
 
@@ -6,14 +13,15 @@ populacaoB = 200000
 
 anos = 0
 
-while populacaoA <= populacaoB:
-    anos+=1
+while populacaoA < populacaoB:
 
     populacaoA += (populacaoA/100)*3
     print(f"populacaoA = {populacaoA}")
 
     populacaoB += (populacaoB/100)*1.5
     print(f"populacaoB = {populacaoB}")
+    anos+=1
+
 
 
 print(f"foram necessario {anos} para a população A igualar a população B")
