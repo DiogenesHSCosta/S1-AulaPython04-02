4c19d9f0c7809c484e75209aba535278bd1eb7c4
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-16 20:35:14 -0300

    inserção enunciado

--- ex7.py
+++ ex7.py
@@ -1,4 +1,10 @@
-#exercicio1
+#exercicio7
+
+'''
+Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número
+inteiro entre 1 a 10. O usuário deve informar de qual numero ele deseja ver a
+tabuada. A saída deve ser conforme o exemplo abaixo:
+'''
 
 j = 1
 while j < 10:
