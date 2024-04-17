21f9edd77d574ee0912529c73e5f7b79977577b5
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-16 21:55:18 -0300

    Melhoria código ex10

--- ex11.py
+++ ex11.py
@@ -5,13 +5,13 @@ while not num.isnumeric():
 num = int(num)
 i = 2
 
-while i < num:
+while i < num/2:
     print(f'{num}%{i} = {num%i}')
     if num % i == 0:
         print(f"o numero {num} não é primo")
         break
     i+=1
 
-if i == num:
+if i == num/2:
     print(f"o numero {num} é primo")
 
