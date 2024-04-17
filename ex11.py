355b5f8dab786c62346209fef55186715ba69dce
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-16 22:09:49 -0300

    Melhoria código ex11

--- ex11.py
+++ ex11.py
@@ -4,14 +4,14 @@ while not num.isnumeric():
 
 num = int(num)
 i = 2
-
-while i < num/2:
+#só precisamos testar até a raiz de um numero
+while i < num**0.5:
     print(f'{num}%{i} = {num%i}')
     if num % i == 0:
         print(f"o numero {num} não é primo")
         break
     i+=1
 
-if i == num/2:
+if i == num**0.5:
     print(f"o numero {num} é primo")
 
